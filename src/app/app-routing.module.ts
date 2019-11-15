72d329c27ab456f774b778b98f3c11e263a082f9
david falcon <david.026697@gmail.com>
2019-11-15 17:31:57 -0300

    david 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Pagina2Component } from './components/shared/pagina2/pagina2.component';
 import { ContentComponent } from './components/main/content/content.component';
+import {LorenaComponent} from './components/shared/lorena/lorena/lorena.component';
+import { AlonsoComponent } from './components/shared/alonso/alonso.component';
 import { XimenaComponent } from './components/shared/ximena/ximena/ximena.component';
 import { SebastianComponent } from './components/shared/sebastian/sebastian/sebastian.component';
 import { AndyComponent } from './components/shared/andy/andy/andy.component';
@@ -11,25 +13,35 @@ import { CristabelComponent } from './components/shared/cristabel/cristabel.comp
 import { NelsonComponent } from './components/shared/nelson/nelson/nelson.component';
 import { RodrigoComponent } from './components/shared/rodrigo/rodrigo/rodrigo.component';
 import { GeregereComponent } from './components/shared/carlos/geregere.component';
+import { HernanComponent } from './components/shared/hernan/hernan/hernan.component';
+import { DavidFalconComponent } from './components/shared/david/david-falcon/david-falcon.component';
+import { OliverComponent } from './components/shared/oliver/oliver.component';
+import { ErickComponent } from './components/shared/erick/erick/erick.component';
+import { FabianComponent } from './components/shared/fabian/fabian/fabian.component';
 
-const routes: Routes = [
+const router: Routes = [
   { path: 'content', component: ContentComponent },
   { path: 'ximena', component: XimenaComponent },
   { path: 'sebastian', component: SebastianComponent },
   { path: 'andy', component: AndyComponent },
-  { path: 'bastian', component: BastianComponent },
   { path: 'camilo', component: CamiloComponent },
   { path: 'cristabel', component: CristabelComponent },
   { path: 'nelson', component: NelsonComponent },
   { path: 'rodrigo', component: RodrigoComponent },
   { path: 'carlos', component: GeregereComponent },
-  { path: 'cristabel', component: CristabelComponent },
-  { path: 'cristabel', component: CristabelComponent },
+  { path: 'hernan', component: HernanComponent },
+  { path: 'david', component: DavidFalconComponent },
+  { path: 'oliver', component: OliverComponent },
+  { path: 'alonso', component: AlonsoComponent },
+  { path: 'lorena', component: LorenaComponent},
+  { path: 'bastian', component: BastianComponent},
+  { path: 'erick', component: ErickComponent},
+  { path: 'fabian', component: FabianComponent},
   { path: '**', component: ContentComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(router)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
