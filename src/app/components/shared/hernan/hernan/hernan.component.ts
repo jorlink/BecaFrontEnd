4bb47fd37b4cf337935ac2b24e0fcb067155773d
hernanpuellesdev <57403100+hernanpuellesdev@users.noreply.github.com>
2019-11-15 14:59:57 -0300

    1.0

---
+++ src/app/components/shared/hernan/hernan/hernan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hernan',
+  templateUrl: './hernan.component.html',
+  styleUrls: ['./hernan.component.css']
+})
+export class HernanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
