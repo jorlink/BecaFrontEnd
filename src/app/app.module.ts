1665d728c455cef8766ba39e7bdb673faacf5235
RodrigoMedina115 <rodrigo.medina@alumnos.upla.cl>
2019-11-15 15:12:23 -0300

    v1rodrigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { Pagina2Component } from './components/shared/pagina2/pagina2.component'
 import { AppRoutingModule } from './app-routing.module';
 import { FormComponent } from './components/shared/form/form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RodrigoComponent } from './components/shared/rodrigo/rodrigo/rodrigo.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ContentComponent,
     MenuComponent,
     Pagina2Component,
-    FormComponent
+    FormComponent,
+    RodrigoComponent
   ],
   imports: [
     BrowserModule,
