132c6676a8b53784b13819f27e779bb076df54e9
david falcon <david.026697@gmail.com>
2019-11-18 14:57:51 -0300

    segudo

---
+++ src/app/components/shared/david/david-falcon/david-falcon.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { ServicioPruebaService } from '../../../../services/servicio-prueba.service';
+
+@Component({
+  selector: 'app-david-falcon',
+  templateUrl: './david-falcon.component.html',
+  styleUrls: ['./david-falcon.component.css']
+})
+export class DavidFalconComponent implements OnInit {
+paises: any;
+resultados: any;
+mostrar = false;
+valor: string;
+ocultar = false;
+
+  constructor(private serv: ServicioPruebaService) { }
+
+  ngOnInit() {
+this.serv.mostrarPaises().subscribe(resp => {
+  console.log(resp);
+  this.paises = resp;
+  console.log(this.paises)
+});
+
+  }
+
+  
+  buscarPais(valor: string) {
+    // console.log( valor );
+    // tslint:disable-next-line: no-unused-expression
+    this.resultados == '';
+    const resultado: string[] = [];
+    this.valor = valor.toLowerCase();
+    // this.resultado = '';
+    // tslint:disable-next-line: prefer-for-of
+    for ( let i = 0; i < this.paises.length; i++) {
+    const pais = this.paises[i];
+    const nombre = pais.name.toLowerCase();
+    // console.log(nombre);
+    if (nombre.indexOf(this.valor) >= 0 ) {
+
+     resultado.push(pais);
+   this.resultados = resultado;
+      console.log(resultado);
+     this.mostrar = true; 
+} else {
+  if( valor == ''){
+    console.log('no debi entrar');
+    this.ocultar = true;
+  }
+  }
+  
+
+
+  }
+  }
+
+}
